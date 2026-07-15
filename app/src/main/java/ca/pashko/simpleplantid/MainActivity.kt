87685e447664e2e5e4f06e1e1ffe405df7f3c905
dpashko <viper.ca082@outlook.com>
2026-07-14 21:08:26 -0600

    Better layout

--- app/src/main/java/ca/pashko/simpleplantid/MainActivity.kt
+++ app/src/main/java/ca/pashko/simpleplantid/MainActivity.kt
@@ -54,7 +54,13 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			// Add system bar insets to the existing 24dp padding we set in XML
+			v.setPadding(
+				systemBars.left,
+				systemBars.top,
+				systemBars.right,
+				systemBars.bottom
+			)
 			insets
 		}
 		
